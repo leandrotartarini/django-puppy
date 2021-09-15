5b75f435857cd1bbb3ef22e378f68ff26f95aebc
Leandro <s2it_lpinto@uolinc.com>
2021-09-15 07:12:39 +0100

    Created a test view for puppies

---
+++ puppies/tests/test_views.py
@@ -0,0 +1,10 @@
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from ..models import Puppy
+from ..serializers import PuppySerializer
+
+# Initialize the APIClient app
+
+client = Client()
